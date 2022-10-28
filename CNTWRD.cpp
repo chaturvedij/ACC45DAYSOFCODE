1dbf00f04a9342f8ec157c6921e64343e1957c44
chaturvedij <113759547+chaturvedij@users.noreply.github.com>
2022-10-28 09:38:23 +0530

    Add files via upload

---
+++ CNTWRD.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int n,m,z;
+	    cin>>n>>m;
+	    
+	    z=n*m;
+	    cout<<z<<endl;
+	}
+	return 0;
+}
