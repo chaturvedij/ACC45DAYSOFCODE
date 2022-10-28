45dce4579719dfc26aa4de7667621fc71b726580
chaturvedij <113759547+chaturvedij@users.noreply.github.com>
2022-10-28 09:38:52 +0530

    Add files via upload

---
+++ PRIZEPOOL.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int x,y,z;
+	    cin>>x>>y;
+	    
+	    z=10*x+90*y;
+	    
+	    cout<<z<<endl;
+	}
+	return 0;
+}
