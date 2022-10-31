1091a35db8dae855b1bb647b214a4e5581ec3966
chaturvedij <113759547+chaturvedij@users.noreply.github.com>
2022-10-31 14:23:44 +0530

    Add files via upload

---
+++ CHESSTIME.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    int x=n*60;
+	    cout<<x/20<<endl;
+	 }
+	return 0;
+}
