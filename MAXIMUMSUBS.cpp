efdf2447432d52d56cb08a5804e4dc006278b368
chaturvedij <113759547+chaturvedij@users.noreply.github.com>
2022-11-09 07:51:17 +0530

    Add files via upload

---
+++ MAXIMUMSUBS.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int x,z;
+        
+        cin>>x;
+        
+        z=x*60/30;
+        cout<<z<<endl;
+    }
+	return 0;
+}
