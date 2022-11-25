5132c3f531b6e451ba9d59e4014d7fbe79965733
chaturvedij <113759547+chaturvedij@users.noreply.github.com>
2022-11-25 07:58:56 +0530

    Add files via upload

---
+++ JASSIGNMENTS.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    
+	    if(10-x>=3)
+	    {
+	        cout<<"yes"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"no"<<endl;
+	    }
+	}
+	return 0;
+}
