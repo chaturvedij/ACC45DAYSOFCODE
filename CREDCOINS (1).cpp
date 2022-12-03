dad158f456c63e66542697c22bdd2ac6222ef92a
chaturvedij <113759547+chaturvedij@users.noreply.github.com>
2022-12-03 14:58:31 +0530

    Add files via upload

---
+++ CREDCOINS (1).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    int z=x*y;
+	    cout<<z/100<<endl;
+	}
+	return 0;
+}
