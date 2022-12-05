8cc8aec9ded38a6e3ebb37168fb26ae62962ce49
chaturvedij <113759547+chaturvedij@users.noreply.github.com>
2022-12-05 08:07:24 +0530

    Add files via upload

---
+++ HELIUM3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int a,b,x,y;
+	    cin>>a>>b>>x>>y;
+	    
+	    if(a*b<=x*y)
+	    {
+	        cout<<"yes"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"no"<<endl;
+	    }
+	}
+	return 0;
+}
