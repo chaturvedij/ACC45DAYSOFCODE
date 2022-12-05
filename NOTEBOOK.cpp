761ac8bd2af8bfe3d47d3d4ee0c159d4820b1000
chaturvedij <113759547+chaturvedij@users.noreply.github.com>
2022-12-05 08:10:26 +0530

    Add files via upload

---
+++ NOTEBOOK.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    int z=n*1000/100;
+	    cout<<z<<endl;
+	}
+	return 0;
+}
